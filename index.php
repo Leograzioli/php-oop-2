d24d3d0386b531ac6ff63c614df2caa102307fa9
Leograzioli <leograzioli@gmail.com>
2022-12-12 17:06:12 +0100

    set database as required_once

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/models/Category.php";
 require_once __DIR__ . "/models/Food.php";
 require_once __DIR__ . "/models/Toy.php";
 require_once __DIR__ . "/models/Kennel.php";
-require __DIR__ . "/database/Products.php";
+require_once __DIR__ . "/database/Products.php";
 
 ?>
 
